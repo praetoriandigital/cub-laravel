c796dd02f0c1914d3e4b9104cadcdaeb9a1fa9aa
Jonny Pickett <jonathanpickett@mac.com>
2018-03-02 09:59:52 -0800

    Update to use current cub widget js

--- tests/Cub/CubLaravel/Test/CubWidgetTest.php
+++ tests/Cub/CubLaravel/Test/CubWidgetTest.php
@@ -13,7 +13,7 @@ class CubWidgetTest extends CubLaravelTestCase
                   . 'var firstScript = document.getElementsByTagName("script")[0];'
                   . 'var cubJs = document.createElement("script");'
                   . 'cubJs.id = "cub-widget-script";'
-                  . 'cubJs.src = "//cub-praetorian.netdna-ssl.com/cub-widget.0.17.x.js";'
+                  . 'cubJs.src = "//cub-praetorian.netdna-ssl.com/cub-widget.0.18.x.js";'
                   . 'firstScript.parentNode.insertBefore(cubJs, firstScript);'
                   . '}());'
                   . '</script>';
