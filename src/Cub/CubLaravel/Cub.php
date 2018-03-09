8b9756302407cbd261bc9b686df903787e64ee99
Jonny Pickett <jonathanpickett@mac.com>
2018-03-09 07:25:40 -0800

    Pass Cub_Object to process object in updateMemberPermissions

--- src/Cub/CubLaravel/Cub.php
+++ src/Cub/CubLaravel/Cub.php
@@ -558,10 +558,9 @@ class Cub
             $result = Cub_Api::post('members/'.$appObject->cub_id.'/permissions', $params);
             $comparisonArray = array_intersect_key($result, $params);
             if ($comparisonArray == $params) {
-                return $this->processObject([
-                    'object' => 'member',
-                    'id' => $appObject->cub_id,
-                ]);
+                $cubObject = $this->convertAppObject($appObject);
+                $instance = new $cubObject(['id' => $appObject->cub_id]);
+                return $this->processObject($instance);
             }
         }
 
